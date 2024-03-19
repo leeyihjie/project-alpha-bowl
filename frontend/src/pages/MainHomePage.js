06a644efd333730fe168565b615e6c0821ea533f
Lee Yih Jie <leeyihjie@yahoo.com.sg>
2024-03-19 18:42:47 +0800

    Basic frames of a game done

--- frontend/src/pages/MainHomePage.js
+++ frontend/src/pages/MainHomePage.js
@@ -1,11 +1,48 @@
 import React from "react";
+import Box from "@mui/material/Box";
 
 const MainHomePage = () => {
   return (
     <>
-      <div className="MainHomePage-container">
-        <h2>Welcome. Learn how to maximise your bowling score here with our bowling score calculator.</h2>
-        <
+      <div className="main-container">
+        <h2>
+          Welcome. Learn how to maximise your bowling score here with our
+          bowling score calculator.
+        </h2>
+        <div className="bowling-frames">
+          <div className="indi-frame">
+            <div id="frame1">
+              <Box
+                sx={{
+                  border: "2px solid red",
+                  width: 50,
+                  height: "50%",
+                }}
+              >
+                1
+              </Box>
+              <Box
+                sx={{
+                  border: "2px solid red",
+                  width: 50,
+                  height: "50%",
+                }}
+              >
+                2
+              </Box>
+              <Box
+                sx={{
+                  border: "2px solid red",
+                  height: "50%",
+                  width: "50%",
+                  flex: 1,
+                }}
+              >
+                3
+              </Box>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
