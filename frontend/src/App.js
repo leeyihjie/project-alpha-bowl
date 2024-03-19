7c07c1b75e7c1448c15caa74cc4204f545b565dc
Lee Yih Jie <leeyihjie@yahoo.com.sg>
2024-03-19 15:32:29 +0800

    created backbone of login page, API connection function, and certain application routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <WelcomePage></WelcomePage>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<WelcomePage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
