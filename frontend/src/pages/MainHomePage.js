5d2ec4067c8fbdaba3e2b66aba89e317256e0a41
Lee Yih Jie <leeyihjie@yahoo.com.sg>
2024-03-19 16:58:22 +0800

    ...

--- frontend/src/pages/MainHomePage.js
+++ frontend/src/pages/MainHomePage.js
@@ -1,14 +1,7 @@
 import React from "react";
 
 const MainHomePage = () => {
-<<<<<<< HEAD
-  return;
-  <>
-    <div></div>
-  </>;
-=======
   return <></>;
->>>>>>> RegisterPage
 };
 
 export default MainHomePage;
